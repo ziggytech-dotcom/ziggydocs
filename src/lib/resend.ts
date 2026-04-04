7099925bccbaf41134a76ec18b6e694c89098a65
Jordan Etzig <jordan@ziggytechventures.com>
2026-04-04 04:00:31 +0000

    fix: lazy-init Resend client in lib/resend.ts to prevent build errors

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,3 +1,11 @@
 import { Resend } from 'resend'
 
-export const resend = new Resend(process.env.RESEND_API_KEY!)
+let _resend: Resend | null = null
+export const resend = {
+  emails: {
+    send: (...args: Parameters<Resend['emails']['send']>) => {
+      if (!_resend) _resend = new Resend(process.env.RESEND_API_KEY!)
+      return _resend.emails.send(...args)
+    }
+  }
+}
